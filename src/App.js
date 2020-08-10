90c040946715b7b9b906839170f723ce0ee77766
Your Name <you@example.com>
2020-08-10 21:51:06 +0430

    asdadad

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import './App.css';
 import Person from './Person/Person'
+import Radium from 'radium';
 
 class App extends Component {
     state = {
@@ -51,8 +52,12 @@ class App extends Component {
             font: 'inherit',
             border: '1px solid blue',
             padding: '8px',
-            cursor: 'pointer'
-        }
+            cursor: 'pointer',
+            ':hover' : {
+                backgroundColor : 'lightgreen',
+                color : 'black'
+            }
+        };
 
         let persons = null;
 
@@ -72,6 +77,10 @@ class App extends Component {
             );
 
             style.backgroundColor = 'red';
+            style[':hover'] = {
+                backgroundColor: 'salmon',
+                color: 'black'
+            }
         }
 
         const classes = [];
@@ -98,4 +107,4 @@ class App extends Component {
     }
 }
 
-export default App;
+export default Radium(App);
